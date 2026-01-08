9c4e980cb29f5557f2c262cf798006c80a077074
caliniya <caliniya@outlook.com>
2026-01-09 00:42:11 +0800

    update

--- core/src/caliniya/armavoke/world/Floor.java
+++ core/src/caliniya/armavoke/world/Floor.java
@@ -6,23 +6,19 @@ import caliniya.armavoke.base.game.ContentType;
 import caliniya.armavoke.base.type.CType;
 
 public class Floor extends ContentType {
-    
-    // 【新增】存储纹理区域，避免每次渲染都调用 Core.atlas.find
-    public TextureRegion region;
 
-    public Floor(String name){
-        super(name, CType.Floor);
+  public TextureRegion region;
+
+  public Floor(String name) {
+    super(name, CType.Floor);
+  }
+
+  public void load() {
+
+    region = Core.atlas.find(name);
+
+    if (!Core.atlas.isFound(region)) {
+      // region = Core.atlas.find("error");
     }
-    
-    // 【新增】资源加载方法
-    // 请在游戏启动的 LoadContent 阶段调用此方法
-    public void load() {
-        // 查找与 name 同名的纹理
-        region = Core.atlas.find(name);
-        
-        // 可选：如果没找到，使用默认的 "error" 纹理，防止空指针
-        if (!Core.atlas.isFound(region)) {
-            // region = Core.atlas.find("error"); 
-        }
-    }
+  }
 }
