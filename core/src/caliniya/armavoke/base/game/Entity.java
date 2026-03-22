d68eaacc5df4441de78f0f680ed66f740f78fa76
caliniya <caliniya@outlook.com>
2026-03-23 00:28:19 +0800

    updata

--- core/src/caliniya/armavoke/base/game/Entity.java
+++ core/src/caliniya/armavoke/base/game/Entity.java
@@ -1,5 +1,28 @@
 package caliniya.armavoke.base.game;
 
-public interface Entity {
-	
+import arc.util.Nullable;
+import arc.util.pooling.Pool.Poolable;
+import caliniya.armavoke.base.type.TeamTypes;
+import caliniya.armavoke.game.data.TeamData;
+
+public abstract class Entity {
+  
+  //瓦片坐标
+  public int tx , ty ;
+  
+  //像素坐标
+  public float x, y;
+  
+  public float health,maxHealth;
+  
+  public int id;
+  public TeamTypes team;
+  public TeamData teamData;
+  
+  public void hit(){
+    hit(null);
+  }
+  public abstract void hit(@Nullable Entity origin);
+
+  public abstract void heal();
 }
