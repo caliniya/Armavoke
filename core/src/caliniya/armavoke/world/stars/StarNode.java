4339018f7ad9b4a9f56a93a650ed0274daac9525
caliniya <caliniya@outlook.com>
2026-07-31 00:23:15 +0800

    updata

--- core/src/caliniya/armavoke/world/stars/StarNode.java
+++ core/src/caliniya/armavoke/world/stars/StarNode.java
@@ -1,6 +1,16 @@
 package caliniya.armavoke.world.stars;
 
-//表示宇宙中的一个星系节点
-public class StarNode {
-	
+import arc.math.geom.QuadTree.QuadTreeObject;
+import arc.math.geom.Rect;
+import caliniya.armavoke.base.api.DrawType;
+
+// 表示宇宙中的一个星系节点
+public class StarNode implements QuadTreeObject {
+
+  public float x, y, size;
+
+  @Override
+  public void hitbox(Rect box) {
+    box.set(x, y, size, size);
+  }
 }
