742bfb08e6e05d86742ba3820773771c60a241cb
caliniya <caliniya@outlook.com>
2026-08-26 02:48:46 +0800

    updata

--- core/src/caliniya/armavoke/base/api/ApiTest.java
+++ core/src/caliniya/armavoke/base/api/ApiTest.java
@@ -1,6 +1,7 @@
 package caliniya.armavoke.base.api;
 
 import caliniya.armavoke.annotation.Annotations.*;
+import caliniya.armavoke.base.anno.auto.AnnoProc;
 
 @SystemDef(name = "aaa" , thread = "aaa" , index =5)
 @Component(name = "test")
