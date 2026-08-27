ed390aecac99202778d79bfd1e965cd92c468078
caliniya <caliniya@outlook.com>
2026-08-28 02:39:22 +0800

    updata

--- annotation/src/main/java/caliniya/vergvoke/annotation/ecs/ECProcessor.java
+++ annotation/src/main/java/caliniya/vergvoke/annotation/ecs/ECProcessor.java
@@ -2,19 +2,19 @@ package caliniya.vergvoke.annotation.ecs;
 
 import arc.struct.ObjectMap;
 import arc.struct.ObjectSet;
+import caliniya.vergvoke.annotation.Annotations.*;
+import caliniya.vergvoke.annotation.Processor;
 import caliniya.vergvoke.annotation.tool.AElement;
 import caliniya.vergvoke.annotation.tool.AType;
 import caliniya.vergvoke.annotation.tool.AVar;
+import caliniya.vergvoke.base.anno.auto.AnnoProc;
 import caliniya.vergvoke.base.tool.Ar;
+import java.util.*;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
 import javax.tools.Diagnostic;
 import javax.tools.StandardLocation;
-import java.util.*;
-import caliniya.vergvoke.annotation.Processor;
-import caliniya.vergvoke.annotation.Annotations.*;
-import caliniya.vergvoke.base.anno.auto.AnnoProc;
 
 @AnnoProc
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
@@ -37,11 +37,16 @@ public class ECProcessor extends Processor {
     entityDef.addAll(types(Entity.class));
 
     for (AType a : entityDef) {
-      ECMap.put(a.annotation(Entity.class).name() + "Entity", new ObjectSet<String>());
+      ObjectSet<String> map = new ObjectSet<String>();
+      for (Class<?> c : a.annotation(Entity.class).comps()) {
+        if (map.contains(c.getSimpleName())) {
+          error(c.getSimpleName() + "Already exist");
+        }
+        map.add(c.getSimpleName());
+      }
+      ECMap.put(a.annotation(Entity.class).name() + "Entity", map);
     }
-    
-    
-    
+
     for (AType T : types(Component.class)) {
       for (AVar V : T.fields()) {
         /**
